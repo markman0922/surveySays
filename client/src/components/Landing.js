e50d9a8da7b660ef0aecad007ac88ab8f3c5b80d
Mark Mandell <markman0922@gmail.com>
2018-07-26 13:15:51 -0700

    addition of client side

---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+  return(
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Survey Says: Landing Page!
+      </h1>
+      <h5>
+        Feedback for businesses. Surveys for scientific research.
+      </h5>
+    </div>
+  );
+};
+
+export default Landing;
